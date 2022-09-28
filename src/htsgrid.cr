d8c1c2fde58087da638acdeee4ee7779131498b3
kojix2 <2xijok@gmail.com>
2022-09-29 08:33:28 +0900

    Update

--- src/htsgrid.cr
+++ src/htsgrid.cr
@@ -4,14 +4,34 @@ require "hts"
 HTS_VERSION = String.new(HTS::LibHTS.hts_version)
 
 def activate(app : Gtk::Application)
-  builder = Gtk::Builder.new_from_file("#{__DIR__}/../data/tree.cmb.ui")
+  builder = Gtk::Builder.new_from_file("#{__DIR__}/../data/tree.ui")
   list_model = Gtk::ListStore.cast(builder["list_model"])
-  itr = list_model.append()
-  list_model.set(itr, {0}, {"Hello"})
+  fill_model(list_model)
   window = Gtk::ApplicationWindow.cast(builder["window"])
   window.application = app
   tree_view = Gtk::TreeView.cast(builder["tree_view"])
   window.present
+end
+
+def fill_model(model : Gtk::ListStore)
+  HTS::Bam.open("/home/kojix2/Ruby/ruby-htslib/test/fixtures/poo.sort.bam") do |bam|
+    bam.each do |r|
+      itr = model.append
+      row = [
+        r.qname,
+        r.flag.to_s,
+        r.chrom,
+        (r.pos + 1).to_s,
+        (r.mapq).to_s,
+        r.cigar.to_s,
+        r.mate_chrom,
+        (r.mpos + 1).to_s,
+        r.isize.to_s,
+        r.seq,
+      ]
+      model.set(itr, (0..10), row)
+    end
+  end
 end
 
 app = Gtk::Application.new("htsgrid.kojix2.com", Gio::ApplicationFlags::None)
