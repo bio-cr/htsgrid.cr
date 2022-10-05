88d4c2b04dd922fcb3155e6166d21d88ca6e6872
kojix2 <2xijok@gmail.com>
2022-10-05 09:49:22 +0900

    Add FileChooserDialog

--- src/htsgrid/view/main_window.cr
+++ src/htsgrid/view/main_window.cr
@@ -18,6 +18,10 @@ module HTSGrid
         @list_model ||= Gtk::ListStore.cast(builder["list_model"])
       end
 
+      def window
+        @window ||= window = Gtk::ApplicationWindow.cast(builder["window"])
+      end
+
       def activate(app : Gtk::Application)
         open_button = Gtk::Button.cast(builder["open_button"])
         entry = Gtk::Entry.cast(builder["entry"])
@@ -25,37 +29,38 @@ module HTSGrid
           open_button_clicked(entry.text)
         end
         HTSGrid::Action::About.new(app)
-        window = Gtk::ApplicationWindow.cast(builder["window"])
         window.application = app
         tree_view = Gtk::TreeView.cast(builder["tree_view"])
         window.present
       end
 
       def open_button_clicked(path : String)
-        file_path = File.expand_path(path, home: Path.home)
-        list_model.try do |model|
-          fill_model(model, file_path)
-        end
+        file_chooser_dialog
       end
 
-      def get_file_path(app)
-        file_path = ""
-        # dialog = Gtk::FileChooserDialog.new(application: app, title: "Open File", action: :open)
-        # dialog.add_button("Cancel", Gtk::ResponseType::Cancel.value)
-        # dialog.add_button("Open", Gtk::ResponseType::Accept.value)
-        # dialog.response_signal.connect do |response|
-        #   case Gtk::ResponseType.from_value(response)
-        #   when .cancel?
-        #   when .accept?
-        #     file_path = dialog.file.try(&.path)
-        #     puts "File selected: #{file_path}"
-        #   end
-        #   dialog.destroy
-        # end
-        # dialog.show
-        # dialog.visible = true
-        # dialog.present
-        file_path
+      def file_chooser_dialog
+        dialog = Gtk::FileChooserDialog.new(
+          application: @app,
+          title: "Open File",
+          action: Gtk::FileChooserAction::Open,
+          transient_for: window,
+          modal: true
+        )
+        dialog.add_button("Cancel", Gtk::ResponseType::Cancel.value)
+        dialog.add_button("Open", Gtk::ResponseType::Accept.value)
+        dialog.response_signal.connect do |response|
+          case Gtk::ResponseType.from_value(response)
+          when .cancel?
+          when .accept?
+            file_path = dialog.file.try(&.path)
+            unless file_path.nil?
+              file_path = File.expand_path(file_path, home: Path.home)
+              list_model.try { |model| fill_model(model, file_path) }
+            end
+          end
+          dialog.destroy
+        end
+        dialog.present
       end
 
       def fill_model(model : Gtk::ListStore, file_path)
